abc8f3d94e72771bbe7e04cf3b66853a52e90bf4
Anton Elshin <anton.elshin@mail.ru>
2023-09-06 22:32:44 +0300

    init db + api yaml with class api

---
+++ rcs-core-service/src/main/java/ru/rcs/RcsCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.rcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcsCoreServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RcsCoreServiceApplication.class, args);
+  }
+
+}
---
+++ rcs-core-service/src/test/java/ru/rcs/RcsCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcsCoreServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
